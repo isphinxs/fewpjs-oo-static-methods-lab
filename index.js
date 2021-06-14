eb0c203ab7de2642a84e8f23f989f1b056db2d70
Samantha Ostrowski <isphinxs@hotmail.com>
2021-06-13 21:25:47 -0400

    commit tree 5f6aef1c9518de6702df968b02517beabd51f372

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-\'\ ']+/g, '');
+  }
+
+  static titleize(string) {
+    const exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    const words = string.split(" ");
+    const capitalizedWords = words.map((word, index) => {
+      if (index > 0 && exceptions.includes(word)) {
+        return word;
+      }
+      return Formatter.capitalize(word);
+    })
+    return capitalizedWords.join(" ");
+  }
 }
